b2777bbe22491dda8e0726c7e414dd19fab77193
Chely Ho <adept_inept@yahoo.com>
2018-02-11 17:40:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,4 @@
 def Song(name, artist, genre)
-  attr_accessor :name, :artist, :genre
-
-  @@count = 0
-
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-
-    @@count += 1
+  def initialize
   end
 end
