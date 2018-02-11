5e2e7d2a0771f55331f4c322f5c5a1bd7e1f1800
Chely Ho <adept_inept@yahoo.com>
2018-02-11 17:46:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,20 @@
 def Song
   attr_accessor :name, :artist, :genre
 
+  @@genres = []
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
 end
+
+:@@artists, [])
+Song.class_variable_set(:@@genres, [])
+Song.class_variable_set(:@@count, 0)
+Song.class_variable_set(:@@artist_count, {})
+Song.class_variable_set(:@@genre_count, {})
